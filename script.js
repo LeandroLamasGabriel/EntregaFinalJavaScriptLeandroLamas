4b528cd4537009814fa34319da47d6326d5c73bc
Leandro Lamas <lean2k19@hotmail.com>
2024-03-24 16:36:10 -0300

    Entrega Final JavaScript Leandro Lamas

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-function obtenerProductos() {
-    return [
-        { id: 1, nombre: "Ryzen 3", categoria: "Procesadores-Ryzen", stock: 10, precio: 30000, url: "../images/Ryzen3.jpeg" },
-        { id: 3, nombre: "Ryzen 5", categoria: "Procesadores-Ryzen", stock: 8, precio: 50000, url: "../images/Ryzen5.jpg" },
-        { id: 5, nombre: "Ryzen 7", categoria: "Procesadores-Ryzen", stock: 5, precio: 70000, url: "../images/Ryzen7.jfif" },
-        { id: 7, nombre: "Kingston 8gb", categoria: "Memoria-Ram", stock: 10, precio: 8000, url: "../images/Ram8.jpg" },
-        { id: 9, nombre: "Kingston 16gb", categoria: "Memoria-Ram", stock: 8, precio: 16000, url: "../images/Ram16.jfif" },
-        { id: 11, nombre: "Kingston 32gb", categoria: "Memoria-Ram", stock: 5, precio: 32000, url: "../images/Ram32.jpg" },
-        { id: 13, nombre: "Disco Duro 500gb", categoria: "HDD", stock: 10, precio: 5000, url: "../images/HDD500.jfif" },
-        { id: 15, nombre: "Disco Duro 1TB", categoria: "HDD", stock: 8, precio: 10000, url: "../images/HDD1TB.jfif" },
-        { id: 17, nombre: "Disco Duro 2TB", categoria: "HDD", stock: 5, precio: 20000, url: "../images/HDD2TB.jpeg" },
-        { id: 19, nombre: "Intel i3", categoria: "Procesadores-Intel", stock: 10, precio: 30000, url: "../images/Intel3.jfif" },
-        { id: 21, nombre: "Intel i5", categoria: "Procesadores-Intel", stock: 8, precio: 50000, url: "../images/Intel5.jpg" },
-        { id: 23, nombre: "Intel i7", categoria: "Procesadores-Intel", stock: 5, precio: 70000, url: "../images/Intel7.jpg" }
-    ]
+async function pedirDatosAlBackend() {
+    try {
+        const resp = await fetch("../datos.json")
+        const info = await resp.json()
+        principal(info)
+    } catch (error) {
+        Swal.fire({
+            title: 'Error!',
+            width: '',
+            text: 'Algo salió mal, error: ' + error,
+            icon: 'error',
+            backdrop: 'rgba(0,0,123,0.4)',
+            confirmButtonText: 'Aceptar'
+
+        })
+    }
 }
-principal()
-function principal() {
-    let productos = obtenerProductos()
+
+pedirDatosAlBackend()
+
+function principal(productos) {
     let input = document.getElementById("input")
     let inputPrecioMenor = document.getElementById("precioMenor")
     let inputPrecioMayor = document.getElementById("precioMayor")
@@ -27,7 +29,7 @@ function principal() {
     ver[2] = document.getElementById("Memoria-Ram")
     ver[3] = document.getElementById("HDD")
     let mostrarCarrito = document.getElementById("carrito")
-    mostrarCarrito.addEventListener("click", renderizarCarrito)
+    mostrarCarrito.addEventListener("click", () => renderizarCarrito(productos))
     let botonBuscar = document.getElementById("botonBuscar")
     botonBuscar.addEventListener("click", () => filtrarTarjetas(input, inputPrecioMenor, inputPrecioMayor, productos))
     ver[0].addEventListener("click", () => mostrarProductosSeleccionado(productos, ver[0]))
@@ -80,25 +82,36 @@ function mostrarProductosSeleccionado(productos, ver) {
         `
         contenedor.append(tarjetaProd)
         let botonAgregarAlCarrito = document.getElementById(id)
-        botonAgregarAlCarrito.addEventListener("click", agregarAlCarrito)
+        botonAgregarAlCarrito.addEventListener("click", (e) => agregarAlCarrito(e, productos))
     })
 }
 
-function agregarAlCarrito(e) {
+function agregarAlCarrito(e, productos) {
     let carrito = obtenerCarrito()
     let idBotonProducto = Number(e.target.id)
-    let productoBuscado = obtenerProductos().find(({ id }) => id === idBotonProducto)
+    let productoBuscado = productos.find(({ id }) => id === idBotonProducto)
     let { id, nombre, precio, stock, url } = productoBuscado
     let productoEnCarrito = carrito.find(({ id }) => id === idBotonProducto)
 
     if (stock > 0) {
-        productoBuscado.stock--
         if (productoEnCarrito) {
             if (stock > productoEnCarrito.unidades) {
                 productoEnCarrito.unidades++
                 productoEnCarrito.subtotal = productoEnCarrito.precioUnitario * productoEnCarrito.unidades
+                lanzarTostada()
+            }
+            if (productoEnCarrito.unidades == productoBuscado.stock) {
+                Swal.fire({
+                    title: 'Error!',
+                    width: '',
+                    text: 'No quedan más unidades de este producto',
+                    icon: 'warning',
+                    backdrop: 'rgba(0,0,123,0.4)',
+                    confirmButtonText: 'Aceptar'
+                })
             }
         } else {
+            lanzarTostada()
             carrito.push({
                 id: id,
                 nombre: nombre,
@@ -109,24 +122,36 @@ function agregarAlCarrito(e) {
             })
         }
         localStorage.setItem("carrito", JSON.stringify(carrito))
-    } else {
-        alert("No quedan más unidades del producto")
     }
+
 }
-function renderizarCarrito() {
-    console.log(obtenerCarrito())
+function lanzarTostada() {
+    Toastify({
+        text: "Prodcuto agregado",
+        duration: 3000,
+        gravity: "bottom",
+        style: {
+            background: "linear-gradient(to right, #5a5bf3, #91e7d9 )",
+        }
+    }).showToast();
+}
+function renderizarCarrito(productos) {
     let carrito = obtenerCarrito()
     let btnComprar = document.getElementById("filtrado")
     btnComprar.innerHTML = ""
+
     btnComprar.innerHTML = `
+        <button id="botonVaciar" class="btn btn-primary"> Vaciar Carrito </button>
         <button id="botonComprar" class="btn btn-primary"> COMPRAR </button>
     `
+    let vaciarCarrito = document.getElementById("botonVaciar")
+    vaciarCarrito.addEventListener("click", funcionVaciarCarrito)
     let finalizarCompra = document.getElementById("botonComprar")
-    finalizarCompra.addEventListener("click", finalizarCompras)
+    finalizarCompra.addEventListener("click", () => finalizarCompras(productos))
 
     let contenedor = document.getElementById("Productos")
     contenedor.innerHTML = ""
-
+    
     carrito.forEach(({ nombre, subtotal, unidades, id, url }) => {
         if (unidades != 0) {
             let tarjetaProd = document.createElement("div")
@@ -147,21 +172,64 @@ function renderizarCarrito() {
             botonQuitarAlCarrito.addEventListener("click", quitarAlCarrito)
         }
     })
-}
 
-function finalizarCompras() {
-    alert("Muchas gracias por su compra")
+    if (contenedor.innerHTML == "") {
+        contenedor.innerHTML = `
+            <h5 class="card-title text-center">Su carrito esta vacio</h5>
+            <p class="text-center">Para seguir eligiendo nuestros productos, navegar entre las diferentes categorias</p>
+        `
+    }
+}
+function funcionVaciarCarrito() {
     localStorage.removeItem("carrito")
     renderizarCarrito()
+}
+function finalizarCompras() {
+    let carrito = obtenerCarrito()
+    let total = 0
+    let items = ""
+    carrito.forEach(({ nombre, subtotal, unidades }) => {
+        if (unidades != 0) {
+            total = total + subtotal
+            items = items + nombre + ' x' + unidades + '\n'
+
+            Swal.fire({
+                title: items,
+                text: "TOTAL: " + total,
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Finalizar Compra"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire({
+                        title: "Compra Realizada",
+                        width: 400,
+                        icon: "success",
+                        padding: "3em",
+                        color: "#716add",
+                        backdrop: `
+                        rgba(0,0,123,0.4)
+                        url("/images/nyan-cat.gif")
+                        left top
+                        no-repeat
+                    `
+                    });
+                    localStorage.removeItem("carrito")
+                    renderizarCarrito()
+                }
+            });
+        }
+    })
 }
 
 function quitarAlCarrito(e) {
     let carrito = obtenerCarrito()
     let idBotonProducto = Number(e.target.id)
     let productoEnCarrito = carrito.find(({ id }) => id === idBotonProducto)
-
     if ((e.target.id == productoEnCarrito.id) & (productoEnCarrito.unidades > 0)) {
         productoEnCarrito.unidades--
+        productoEnCarrito.subtotal = productoEnCarrito.subtotal - productoEnCarrito.precioUnitario
         localStorage.setItem("carrito", JSON.stringify(carrito))
     }
     renderizarCarrito()
@@ -174,11 +242,11 @@ function obtenerCarrito() {
 function filtrarTarjetas(inputNombre, inputPrecioMenor, inputPrecioMayor, productos) {
     let productosFiltrados
     if (inputNombre.value && (!inputPrecioMenor.value && !inputPrecioMayor.value)) {
-        productosFiltrados = productos.filter(producto => producto.nombre.includes(inputNombre.value))
+        productosFiltrados = productos.filter(producto => producto.nombre.toUpperCase().includes(inputNombre.value.toUpperCase()))
     } else if (!inputNombre.value && (inputPrecioMenor.value && inputPrecioMayor.value)) {
         productosFiltrados = productos.filter(producto => producto.precio >= Number(inputPrecioMenor.value) && producto.precio <= Number(inputPrecioMayor.value))
     } else if (inputNombre.value && inputPrecioMenor.value && inputPrecioMayor.value) {
-        productosFiltrados = productos.filter(producto => producto.nombre.includes(inputNombre.value) && producto.precio >= Number(inputPrecioMenor.value) && producto.precio <= Number(inputPrecioMayor.value))
+        productosFiltrados = productos.filter(producto => producto.nombre.toUpperCase().includes(inputNombre.value.toUpperCase()) && producto.precio >= Number(inputPrecioMenor.value) && producto.precio <= Number(inputPrecioMayor.value))
     }
     mostrarProductosSeleccionado(productosFiltrados)
 }
